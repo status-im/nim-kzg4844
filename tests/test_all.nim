d17ded0d73fdca98343930d840dc684fc08f888e
jangko <jangko128@gmail.com>
2023-03-06 21:24:49 +0700

    disable yaml test on nim 1.2 and devel

--- tests/test_all.nim
+++ tests/test_all.nim
@@ -10,5 +10,11 @@
 import
   test_abi,
   test_kzg,
-  test_kzg_ex,
-  test_yaml
+  test_kzg_ex
+
+when (NimMajor, NimMinor) >= (1, 4) and 
+     (NimMajor, NimMinor) <= (1, 6):
+  # nim devel causes shallowCopy error
+  # on yaml
+  import
+    test_yaml
